04da7d95c8e9624e4978cbf42a76ba1f02faa3b1
sanketdudhe <sanketd7843@gmail.com>
2020-11-08 18:17:03 +0530

    base url changed

--- app/src/main/java/com/example/aakar/utils/Constants.java
+++ app/src/main/java/com/example/aakar/utils/Constants.java
@@ -1,9 +1,9 @@
 package com.example.aakar.utils;
 
 public class Constants {
-    public static String base_url = "http://houseofbrandz.in/nursing/public/api/";
+    public static String base_url = "https://acmw.net.in/nursing/public/api/";
 
-    public static String base_url_tc = "http://houseofbrandz.in/nursing/public/tc";
+    public static String base_url_tc = "https://acmw.net.in/nursing/public/tc";
 
-    public static String base_url_about = "http://houseofbrandz.in/nursing/public/about";
+    public static String base_url_about = "https://acmw.net.in/nursing/public/about";
 }
